915246e59712c6bf0fa77aec65961084a9302e8f
Kosta Kuprešak <kosta@kupresak.rs>
2023-01-11 21:57:20 +0100

    Add Item entity

---
+++ api/src/main/java/rs/kostakupresak/todo/entities/Item.java
@@ -0,0 +1,27 @@
+package rs.kostakupresak.todo.entities;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "item")
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+    @Column(name = "is_toggled", nullable = false)
+    private Boolean isToggled;
+}
