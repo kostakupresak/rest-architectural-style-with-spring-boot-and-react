cdd966fdbff0c0705bb7bfcef2de7801ab6d3f4c
Kosta Kuprešak <kosta@kupresak.rs>
2023-01-11 23:25:48 +0100

    Add CORS configuration

---
+++ api/src/main/java/rs/kostakupresak/todo/configs/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package rs.kostakupresak.todo.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+    @Value("#{'${todo.cors.allowedOrigins}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    @Value("#{'${todo.cors.allowedMethods}'.split(',')}")
+    private List<String> allowedMethods;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(this.allowedOrigins);
+        configuration.setAllowedMethods(this.allowedMethods);
+
+        http.cors().configurationSource(request -> configuration);
+
+        http.csrf().disable();
+        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                authorizationManagerRequestMatcherRegistry.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
