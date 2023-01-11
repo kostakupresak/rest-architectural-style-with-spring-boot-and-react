c9714bff8e0512ca665e9688648b384f150de20b
Kosta Kuprešak <kosta@kupresak.rs>
2023-01-11 21:47:47 +0100

    Initialize Spring Boot API

---
+++ api/src/test/java/rs/kostakupresak/todo/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.kostakupresak.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
