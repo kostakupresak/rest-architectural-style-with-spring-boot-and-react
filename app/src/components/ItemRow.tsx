1216141e8bd043c266917549883676f2de52828a
Kosta Kuprešak <kosta@kupresak.rs>
2023-01-12 09:18:03 +0100

    Implement update item

--- app/src/components/ItemRow.tsx
+++ app/src/components/ItemRow.tsx
@@ -1,24 +1,32 @@
 import {Item} from "../models/Item";
 
+import itemService from "../services/itemService";
+
 interface ItemRowProps {
   item: Item
+  rerenderItem: (item: Item) => void
 }
 
-function ItemRow({item}: ItemRowProps): JSX.Element {
+function ItemRow({item, rerenderItem}: ItemRowProps): JSX.Element {
   const itemStatusText: string = item.isToggled ? 'Done' : 'Pending';
   const itemStatusClass: string = item.isToggled ? 'badge bg-success' : 'badge bg-warning';
+
+  async function handleToggle(): Promise<void> {
+    const updatedItem: Item = await itemService.toggle(item.id);
+    rerenderItem(updatedItem);
+  }
 
   function ItemToggleButton(): JSX.Element {
     if (item.isToggled) {
       return (
-        <span data-mdb-toggle="tooltip" title="Undo">
+        <span data-mdb-toggle="tooltip" title="Undo" onClick={handleToggle}>
           <img src="images/undo.png" alt="Undo" className="action-icon"/>
         </span>
       );
     }
 
     return (
-      <span data-mdb-toggle="tooltip" title="Done">
+      <span data-mdb-toggle="tooltip" title="Done" onClick={handleToggle}>
         <img src="images/done.png" alt="Done" className="action-icon"/>
       </span>
     );
--- app/src/components/ItemsTable.tsx
+++ app/src/components/ItemsTable.tsx
@@ -1,22 +1,40 @@
 import {useEffect, useState} from "react";
+
 import {Item} from "../models/Item";
-import itemService from "../services/itemService";
 import ItemRow from "./ItemRow";
+
+import itemService from "../services/itemService";
 
 function ItemsTable(): JSX.Element {
   const [items, setItems] = useState<Item[]>([]);
 
   async function loadItems(): Promise<void> {
-    const response = await itemService.getAll();
-    setItems(response);
+    const items: Item[] = await itemService.getAll();
+    setItems(items);
   }
 
   useEffect(() => {
     void loadItems();
   }, []);
 
+  function rerenderItem(item: Item): void {
+    const foundIndex: number = items.findIndex(i => i.id === item.id);
+
+    if (foundIndex < 0) {
+      return;
+    }
+
+    const updatedItems: Item[] = [...items];
+    updatedItems[foundIndex] = item;
+    setItems(updatedItems);
+  }
+
   function ItemRows(): JSX.Element {
-    return <>{items.map(item => <ItemRow key={`item-${item.id}`} item={item}/>)}</>;
+    return (
+      <>
+        {items.map(item => <ItemRow key={`item-${item.id}`} item={item} rerenderItem={rerenderItem}/>)}
+      </>
+    );
   }
 
   return (
